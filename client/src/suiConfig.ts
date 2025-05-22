587bf1c482867ba9986f8cd0781412e699165fb5
kws <kws1207@kaist.ac.kr>
2025-05-22 22:42:47 +0900

    update package ids

--- client/src/suiConfig.ts
+++ client/src/suiConfig.ts
@@ -3,11 +3,11 @@ export const NETWORK = "testnet";
 // TODO: Replace the following values with the actual on-chain object ids once the
 // package is deployed to Devnet. You can find them with the Sui CLI or the explorer.
 export const PACKAGE_ID =
-  "0xdf2ac204ef758ae05809c7171716f5ded8e6182a3f37c71473d69e4444bdcbd7";
+  "0xc382c6d780d654b080bdc16609336ebe15e46a165f65601b388f77e99fb33279";
 export const CONFIG_ID =
-  "0xce5a3bc8d0083ecdfba02bd4078fb37fca8ef7a84b3d361e549662589d72d0d8"; // mizupay::config::ZFubaoConfig shared object
+  "0xc5f9f782a3a9f83107d74bc19d4feae578f556118a36f87df1dbcc886336ecae"; // mizupay::config::ZFubaoConfig shared object
 export const VAULT_ID =
-  "0xc574684ea6e52344d528cba2329d6684769d399a4c6a7090cc10455664bf7e04"; // mizupay::vault::Vault shared object
+  "0x97415cb23af718bf10c61743c35b718a2da855c2f92394714495325745162587"; // mizupay::vault::Vault shared object
 
 // Coin type strings used by the Sui SDK
 export const LBTC_TYPE = `${PACKAGE_ID}::lbtc::LBTC`;
